ef44b360f1f8297107a1d40edb4ebef088027424
Ericsnjy <112568579+Ericsnjy@users.noreply.github.com>
2022-09-08 14:10:29 +0700

    Add files via upload

---
+++ Praktest_Eric Sanjaya Sipayung_kelasc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int A;
+	cout<<"Angka Pertama	:";
+	cin>>A;
+	
+	int B;
+	cout<<"Angka Kedua	:";
+	cin>>B;
+	
+	cout << A + B <<endl;
+	cout << A - B <<endl;
+	cout << A * B <<endl;
+	cout << A / B <<endl;
+	cout << A % B <<endl;
+	
+	return 0;	
+}
