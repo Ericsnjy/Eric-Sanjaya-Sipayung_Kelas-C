a3cebf7b2a828255df4b4f9507d1707d4e5e8f87
Ericsnjy <112568579+Ericsnjy@users.noreply.github.com>
2022-09-22 20:17:05 +0700

    Add files via upload

---
+++ Praktest 2_Eric Sanjaya Sipayung_kelasc.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int nilai;
+	
+	cin >> nilai;
+	if(nilai==1){
+		cout << "Satu\n";
+	}
+	cin >> nilai;
+	if(nilai==2){
+		cout << "Dua\n";
+	}
+	cin >> nilai;
+	if(nilai==3){
+		cout << "Tiga\n";
+	}
+	cin >> nilai;
+	if(nilai>3){
+		cout << "Bilangan Lebih Dari Tiga\n";
+	
+	}
+	
+	return 166;
+}
