064489214ea9ed698b13d6e1aa23e521ee1e2d17
Ericsnjy <112568579+Ericsnjy@users.noreply.github.com>
2022-09-29 13:14:25 +0700

    Add files via upload

---
+++ Praktest 3.cpp
@@ -0,0 +1,16 @@
+//Nama:Eric Sanjaya Sipayung
+//NPM:221705116
+//Kelas:C
+
+#include <iostream>
+using namespace std;
+int main(){
+	int x;int y;
+	cin>>x>>y;
+	for (int i=x ; i<=y ; i+=x){
+		cout << i << "  ";
+	}
+			
+		return 0;
+	}
+
